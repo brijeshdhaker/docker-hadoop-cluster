699110075dd7edfe7de25cd2b107ae41cb97b7ff
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2021-07-16 06:58:48 +0530

    2021-07-16 Update Configuration as per k8s specification

--- docker/hadoop/start-namenode.sh
+++ docker/hadoop/start-namenode.sh
@@ -3,14 +3,14 @@
 # We should format namenode only once. So, when we format it
 # we save a marker file.
 
-if [ ! -f /data/.already_formatted ]; then
+if [ ! -f /apps/hostpath/hadoop/data/.already_formatted ]; then
   echo "Formatting  Name Node"
-  mkdir -p /data/dfs/data
-  mkdir -p /data/dfs/name
-  mkdir -p /data/dfs/namesecondary
+  mkdir -p /apps/hostpath/hadoop/data/dfs/data
+  mkdir -p /apps/hostpath/hadoop/data/dfs/name
+  mkdir -p /apps/hostpath/hadoop/data/dfs/namesecondary
 
   hdfs namenode -format
-  touch /data/.already_formatted
+  touch /apps/hostpath/hadoop/data/.already_formatted
 fi
 
 hdfs namenode
