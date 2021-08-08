fb75dce42b16d95d0091e0b67a49c98123f7fcc0
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2021-08-08 10:28:07 +0530

    2021-08-08 final working hive setup.

---
+++ bigdata-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+#
+#
+
+mkdir -p /apps/hostpath/
+mkdir -p /apps/hostpath/hadoop/yarn
+mkdir -p /apps/hostpath/spark
+mkdir -p /apps/hostpath/postgres
+mkdir -p /apps/hostpath/hive
+mkdir -p /apps/hostpath/hive
+mkdir -p /apps/hostpath/hive
+
+
+if [ ! -f /apps/hostpath/postgres/.already_setup ]; then
+
+fi
