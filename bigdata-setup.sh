8987d42f03a9ce511d4e0bc8da453dd1ddd546a5
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2021-08-08 10:56:06 +0530

    2021-08-08 final working cluster & notebook setup.

--- bigdata-setup.sh
+++ bigdata-setup.sh
@@ -11,8 +11,38 @@ mkdir -p /apps/hostpath/postgres
 mkdir -p /apps/hostpath/hive
 mkdir -p /apps/hostpath/hive
 mkdir -p /apps/hostpath/hive
+mkdir -p /apps/hostpath/notebook
+#
+#
+mkdir -p /apps/hostpath/zeppelin/notebook
+mkdir -p /apps/hostpath/zeppelin/logs
+#
+#
+mkdir -p /apps/hostpath/drivers
+mkdir -p /apps/hostpath/datasets
+#
+#
+#
+mkdir -p /apps/hostpath/localstack
+#
+#
+#
+mkdir -p /apps/hostpath/kafka
+mkdir -p /apps/hostpath/kafka/data
+mkdir -p /apps/hostpath/kafka/schema
 
+#
+#
+#
+if [ -f /apps/hostpath/hadoop ]; then
+  mkdir -p /apps/hostpath/hadoop/yarn
+  mkdir -p /apps/hostpath/hadoop/dfs
+fi
 
-if [ ! -f /apps/hostpath/postgres/.already_setup ]; then
-
+#
+#
+#
+if [ -f /apps/hostpath/hadoop ]; then
+  mkdir -p /apps/hostpath/hadoop/yarn
+  mkdir -p /apps/hostpath/hadoop/dfs
 fi
