d87b44d55f32785380c23e231566a6a48f4f9a90
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2021-12-11 22:07:52 +0530

    2021-12-11 Update configuration.

--- docker-sandbox/bigdata-setup.sh
+++ docker-sandbox/bigdata-setup.sh
@@ -6,13 +6,13 @@
 docker network create -d bridge bigdata.net
 
 #
-# Zookeeper Dirs
+# Require Dirs
+#
+sudo mkdir -p /apps
+sudo chown brijeshdhaker:brijeshdhaker -R /apps
 #
 mkdir -p /apps/hostpath/zookeeper/data
 mkdir -p /apps/hostpath/zookeeper/logs
-
-#
-#
 #
 mkdir -p /apps/hostpath/hadoop/2.7.4/dfs/data
 mkdir -p /apps/hostpath/hadoop/2.7.4/dfs/name
@@ -20,21 +20,14 @@ mkdir -p /apps/hostpath/hadoop/2.7.4/dfs/namesecondary
 
 mkdir -p /apps/hostpath/postgres/hive-2.3.7
 mkdir -p /apps/hostpath/hive/2.3.7
-
-#
 #
 mkdir -p /apps/hostpath/zeppelin/notebook
 mkdir -p /apps/hostpath/zeppelin/logs
 #
-#
 mkdir -p /apps/hostpath/drivers
 mkdir -p /apps/hostpath/datasets
 #
-#
-#
 mkdir -p /apps/hostpath/localstack
-#
-#
 #
 mkdir -p /apps/hostpath/kafka
 mkdir -p /apps/hostpath/kafka/data
