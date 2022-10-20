af57dfc7f086cc0d4d9bfcf4ed69baf510473cc2
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2022-10-20 10:52:24 +0530

    2022-10-20 Update Spark History Server Docker Image configuration.

--- docker-spark/historyserver/startup.sh
+++ docker-spark/historyserver/startup.sh
@@ -6,6 +6,19 @@ echo "Running Spark HistoryServer on ${PUBLIC_DNS}."
 source ${SPARK_HOME}/sbin/spark-config.sh
 source ${SPARK_HOME}/bin/load-spark-env.sh
 
+#
+export PATH=${PATH}:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin
+
+#
+if [ ! -f /apps/sandbox/spark/.already_setup ]; then
+
+  ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p    /tmp/spark/logs
+  echo "Spark History Server log dirs successfully created ."
+
+  touch /apps/sandbox/spark/.already_setup
+
+fi
+
 mkdir -p /spark/logs
 
 ln -sf /dev/stdout /spark/logs/spark-history.out
