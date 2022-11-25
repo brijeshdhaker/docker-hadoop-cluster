c67a9b8f5ffe8effda89b978ac9ee415bff33d44
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2022-11-25 07:57:08 +0530

    2022-11-24 add maven configuration.

--- setup/bigdata-setup.sh
+++ setup/bigdata-setup.sh
@@ -157,3 +157,6 @@ mkdir -p /apps/hostpath/localstack
 
 sudo chown brijeshdhaker:brijeshdhaker -R /apps
 sudo chmod 777 -R /apps
+
+cd /opt
+sudo chown -R brijeshdhaker:root hadoop-3.2.1 hbase-1.1.7 hbase-2.4.9 hive-3.1.2 maven-3.6.3 spark-3.1.2
