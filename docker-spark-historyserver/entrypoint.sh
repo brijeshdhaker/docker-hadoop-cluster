7aa201e1bc2c8631ff2e45d087e356366430f1b5
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2023-02-17 07:36:57 +0530

    2023-02-17 Update Spark Images for K8S & Standalon Setup.

--- docker-spark-historyserver/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ${SPARK_HOME}/bin/load-spark-env.sh
-echo "Starting Spark History Server ... "
-
-. ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.history.HistoryServer --properties-file ${SPARK_HOME}/conf/spark-defaults.conf
