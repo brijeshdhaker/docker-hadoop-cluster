4ac8584001cc22cdd2f524c130453bd1ce7214a5
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2023-02-21 06:21:36 +0530

    2023-02-17 Update Spark Images for K8S & Standalon Setup.

--- docker-hadoop-2.7.2/historyserver/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$HADOOP_HOME/bin/hdfs dfs -mkdir -p /apps/hadoop-2.7.2/yarn/timeline
-$HADOOP_HOME/bin/hdfs dfs -chmod -R 1777 /apps/hadoop-2.7.2/yarn/timeline
-
-$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR historyserver
