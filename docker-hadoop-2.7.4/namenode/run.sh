fd8e87bf367aa39ed3aa45b161d5123340eddcdb
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2023-03-24 07:18:33 +0530

    2023-03-24 add hadoop-2.4.7 docker images.

--- docker-hadoop-2.7.4/namenode/run.sh
+++ docker-hadoop-2.7.4/namenode/run.sh
@@ -15,7 +15,7 @@ fi
 echo "remove lost+found from $namedir"
 #rm -r $namedir/lost+found
 
-if [ ! -f /apps/sandbox/hadoop/.already_formatted ]; then
+if [ ! -f /hadoop/dfs/.already_formatted ]; then
 
   echo "Formatting name node name directory: $namedir"
   mkdir -p /hadoop/dfs/data
@@ -23,7 +23,7 @@ if [ ! -f /apps/sandbox/hadoop/.already_formatted ]; then
   mkdir -p /hadoop/dfs/namesecondary
 
   $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
-  touch /apps/sandbox/hadoop/.already_formatted
+  touch /hadoop/dfs/.already_formatted
 
 fi
 
