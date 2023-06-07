e0ff242958100d10b627db26008f32426c728983
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2023-06-07 23:19:07 +0530

    2023-06-07 Add Kerberos Server & client to sandbox-cluster.

---
+++ docker-kdcclient/init-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source `dirname $0`/setup-client.sh
+
+# Your application
+
+# You can use the `kadminCommand` function to perform kadmin commands. Example:
+# kadminCommand "get_principal yourprincipal@$REALM"
