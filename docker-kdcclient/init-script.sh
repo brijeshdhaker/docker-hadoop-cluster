bd6fc475ffd0de03cc2d5d5ebd700e05e88e3c5d
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2023-06-11 11:11:57 +0530

    2023-06-11 Enable Kerberos for SANDBOX cluster.

--- docker-kdcclient/init-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source `dirname $0`/setup-client.sh
-
-# Your application
-
-# You can use the `kadminCommand` function to perform kadmin commands. Example:
-# kadminCommand "get_principal yourprincipal@$REALM"
