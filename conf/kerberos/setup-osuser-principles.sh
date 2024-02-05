f5bb80d503323c3a730c2261f61919ef446832ba
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2024-02-05 09:31:04 +0530

    2024-02-05 Update OS User kerberos principals.

---
+++ conf/kerberos/setup-osuser-principles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#
+REALM=SANDBOX.NET
+DOMAIN_REALM=sandbox.net
+ROOT_ADMIN_PRINCIPAL=root/admin@SANDBOX.NET
+KADMIN_PRINCIPAL_FULL=kadmin/admin@SANDBOX.NET
+KADMIN_PASSWORD=kadmin
+KUSERS_PASSWORD=kuser
+
+echo ""
+echo "Adding OS users kerberos principals."
+echo ""
+#
+OS_USERS=("root" "brijeshdhaker" "hdfs" "yarn" "mapred" "hive" "hbase" "spark" "zeppelin")
+for ou in "${OS_USERS[@]}"
+do
+  # delete existing keytab files
+  rm -Rf /etc/kerberos/users/$ou.keytab
+  kadmin.local -q "delete_principal -force $ou@$REALM"
+  kadmin.local -q "addprinc -randkey $ou@$REALM"
+  kadmin.local -q "ktadd -k /etc/kerberos/users/$ou.keytab $ou@$REALM"
+  kadmin.local -q "getprinc $ou@$REALM"
+
+  klist -k -t /etc/kerberos/users/$ou.keytab
+  # kinit -k -t /etc/kerberos/users/$ou.keytab $ou@$REALM
+
+  chmod 744 /etc/kerberos/users/$key.keytab
+done
+
+echo ""
+echo "OS users principles password successfully generated."
+echo ""
