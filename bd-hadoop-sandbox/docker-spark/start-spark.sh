71a013dbfc92e928e383de6266b14e02519818f1
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2024-09-05 18:31:10 +0530

    2024-09-05 Update spark configuration.

--- bd-hadoop-sandbox/docker-spark/start-spark.sh
+++ bd-hadoop-sandbox/docker-spark/start-spark.sh
@@ -7,24 +7,24 @@ if [ "${SPARK_WORKLOAD}" == "master" ];
 then
     export SPARK_MASTER_HOST=$(hostname  -i)
     echo "Starting Spark master on ${SPARK_MASTER_HOST}."
-    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master --ip ${SPARK_MASTER_HOST} --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_MASTER_WEBUI_PORT} >> ${SPARK_MASTER_LOG}
+    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master --ip ${SPARK_MASTER_HOST} --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_MASTER_WEBUI_PORT} >> ${SPARK_MASTER_LOG}-$(date +%s).out
 #
 elif [ "${SPARK_WORKLOAD}" == "worker" ];
 then
     export WORKER_HOST=$(hostname  -i)
     echo "Starting Spark worker on $(hostname  -i)."
     # When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
-    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port ${SPARK_WORKER_WEBUI_PORT} ${SPARK_MASTER} >> ${SPARK_WORKER_LOG}
+    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port ${SPARK_WORKER_WEBUI_PORT} ${SPARK_MASTER} >> ${SPARK_WORKER_LOG}-$(date +%s).out
 #
 elif [ "${SPARK_WORKLOAD}" == "HistoryServer" ];
 then
     echo "Spark HistoryServer"
-    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.history.HistoryServer --properties-file ${SPARK_HISTORY_CONF_FILE}
+    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.history.HistoryServer --properties-file ${SPARK_HISTORY_CONF_FILE} >> ${SPARK_HS_LOG}-$(date +%s).out
 #
 elif [ "${SPARK_WORKLOAD}" == "ThriftServer" ];
 then
     echo "Spark ThriftServer"
-    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.SparkSubmit --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 spark-internal
+    ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.SparkSubmit --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 spark-internal >> ${SPARK_TS_LOG}-$(date +%s).out
 #
 elif [ "${SPARK_WORKLOAD}" == "submit" ];
 then
