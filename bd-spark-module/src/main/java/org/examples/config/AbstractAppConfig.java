8af9dcf949742712ecb915112aaf503f392c7df0
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2024-09-15 16:57:59 +0530

    2024-09-15 Update Project Configuration.

--- bd-spark-module/src/main/java/org/examples/config/AbstractAppConfig.java
+++ bd-spark-module/src/main/java/org/examples/config/AbstractAppConfig.java
@@ -1,11 +1,21 @@
 package org.examples.config;
 
+import java.util.Map;
+
 public abstract class AbstractAppConfig {
 
     private Boolean isVerbose;
     private Boolean isEmbedded;
+    private Map<String,String> params;
 
-    public AbstractAppConfig() throws Exception {
+    public AbstractAppConfig(Map<String,String> params) throws Exception {
+        this.params = params;
 
+
+
+
+
+
+        
     }
 }
