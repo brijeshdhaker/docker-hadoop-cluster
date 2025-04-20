4c529db97c4f2e610bb73e296d66d6ff7c6b470c
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2025-04-20 22:34:40 +0530

    2025-04-20 Add Ehcache Support to sb-springboot-module.

--- bd-spring-module/src/main/java/org/examples/sb/config/OpenAPIConfiguration.java
+++ bd-spring-module/src/main/java/org/examples/sb/config/OpenAPIConfiguration.java
@@ -23,6 +23,14 @@ public class OpenAPIConfiguration {
         server.setUrl("http://localhost:9080/api/v1");
         server.setDescription("Development");
 
+        Server k8s_http = new Server();
+        k8s_http.setUrl("http://sbhttps.sandbox.net/api/v1");
+        k8s_http.setDescription("Kubernetes Http");
+
+        Server k8s_https = new Server();
+        k8s_https.setUrl("https://sbhttps.sandbox.net/api/v1");
+        k8s_https.setDescription("Kubernetes Https");
+
         Contact myContact = new Contact();
         myContact.setName("Brijesh K Dhaker");
         myContact.setEmail("brijeshdhaker@gmail.com");
@@ -32,7 +40,7 @@ public class OpenAPIConfiguration {
                 .version("1.0")
                 .description("This API exposes endpoints to manage sandbox APIs.")
                 .contact(myContact);
-        return new OpenAPI().info(information).servers(List.of(server));
+        return new OpenAPI().info(information).servers(List.of(server, k8s_http, k8s_https));
     }
 
 }
